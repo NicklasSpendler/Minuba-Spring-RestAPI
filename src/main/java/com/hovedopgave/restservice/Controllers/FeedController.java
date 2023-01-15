391f5d294981927ac482009a4db7e213e5de3828
LAPTOP-4141SHPJ\seb99 <seb994@live.dk>
2023-01-15 14:39:27 +0100

    crossorigin

--- src/main/java/com/hovedopgave/restservice/Controllers/FeedController.java
+++ src/main/java/com/hovedopgave/restservice/Controllers/FeedController.java
@@ -5,12 +5,14 @@ import com.hovedopgave.restservice.Models.News;
 import com.hovedopgave.restservice.Service.FeedService;
 import com.hovedopgave.restservice.Service.NewsService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@CrossOrigin("*")
 public class FeedController {
 
     @Autowired
